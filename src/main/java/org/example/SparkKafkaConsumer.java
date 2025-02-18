dab5541d1f947d1bf04366f6c73a37b7e3c9d4dc
Khong Hoang Phong <phongkh@vp.local>
2025-02-18 13:35:10 +0700

    test config

--- src/main/java/org/example/SparkKafkaConsumer.java
+++ src/main/java/org/example/SparkKafkaConsumer.java
@@ -20,7 +20,6 @@ public class SparkKafkaConsumer {
 
         // Khởi tạo SparkSession
         SparkSession spark = SparkSession.builder().appName("Testing Spark Consumer")
-                .master("local[*]")
                 .config("deploy-mode", "client")
                .getOrCreate();
         SparkContext sc = spark.sparkContext();
@@ -31,8 +30,8 @@ public class SparkKafkaConsumer {
         Dataset<Row> kafkaStream = spark
                 .readStream()
                 .format("kafka")
-                .option("kafka.bootstrap.servers", "172.31.17.135:9092") // Kafka Server
-                .option("subscribe", "quickstart-events") // Tên topic Kafka
+                .option("kafka.bootstrap.servers", "kafka0:9092") // Kafka Server
+                .option("subscribe", "DEMO3.B2B_SMS.dbo.SMS_MT") // Tên topic Kafka
                 .option("startingOffsets", "earliest") // Đọc từ đầu topic
                 .option("group.id", "testing-v23")
                 .load() ;
@@ -45,23 +44,7 @@ public class SparkKafkaConsumer {
                 .format("console") // Xuất ra console
                 .start();
 
-
-//        Dataset<Row> lines = spark
-//                .readStream()
-//                .format("socket")
-//                .option("host", "172.31.17.135")
-//                .option("port", 9999)
-//                .load();
-//        Dataset<String> words = lines
-//                .as(Encoders.STRING())
-//                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
-
-// Generate running word count
-//        Dataset<Row> wordCounts = words.groupBy("value").count();
-//        StreamingQuery query = wordCounts.writeStream()
-//                .outputMode("complete")
-//                .format("console")
-//                .start();
+          .start();
 
         query.awaitTermination();
     }
